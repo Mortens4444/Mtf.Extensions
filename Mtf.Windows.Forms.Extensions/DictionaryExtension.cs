1ad1350764977305b84f36308d302d771003276c
Mortens <mortens.4444@gmail.com>
2025-09-11 15:29:18 +0200

    Mtf.Extensions NuGet package release 1.0.23

--- Mtf.Windows.Forms.Extensions/DictionaryExtension.cs
+++ Mtf.Windows.Forms.Extensions/DictionaryExtension.cs
@@ -2,30 +2,8 @@
 
 namespace Mtf.Windows.Forms.Extensions
 {
-    public static class DictionaryExtension
+     class DictionaryExtension
     {
-        private static readonly string[] ApplicationDirectories = new string[]
-        {
-                PathProvider.Maps,
-                PathProvider.Music,
-                PathProvider.SoundEffects,
-                PathProvider.Characters
-        };
 
-        public static void CreateApplicationDirectories()
-        {
-            foreach (var directory in ApplicationDirectories)
-            {
-                CreateIfNotExists(directory);
-            }
-        }
-
-        public static void CreateIfNotExists(string directory)
-        {
-            if (!Directory.Exists(directory))
-            {
-                Directory.CreateDirectory(directory);
-            }
-        }
     }
 }
---
+++ Mtf.Windows.Forms.Extensions/Services/DirectoryUtils.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Mtf.Windows.Forms.Extensions.Services
+{
+    public static class DirectoryUtils
+    {
+        private static readonly string[] ApplicationDirectories = new string[]
+        {
+            PathProvider.Maps,
+            PathProvider.Music,
+            PathProvider.SoundEffects,
+            PathProvider.Characters
+        };
+
+        public static void CreateApplicationDirectories()
+        {
+            foreach (var directory in ApplicationDirectories)
+            {
+                CreateIfNotExists(directory);
+            }
+        }
+
+        public static void CreateIfNotExists(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
