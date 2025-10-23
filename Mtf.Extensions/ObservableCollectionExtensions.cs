92bc91a562c7b475909c905c053dab45cae129bb
Mortens <mortens.4444@gmail.com>
2025-10-23 23:37:11 +0200

    Mtf.Extensions NuGet package release 1.0.24

---
+++ Mtf.Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Mtf.Extensions
+{
+    public static class ObservableCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the items to the observable collection one by one.
+        /// Simple and reliable; callers are responsible for making the call on the UI thread if needed.
+        /// </summary>
+        public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    collection.Add(item);
+                }
+            }
+        }
+    }
+
+}
